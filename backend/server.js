6cb510a38c18c78a382d77aca01b142f26808883
Kawser Hossain <109532781+Mkkawser@users.noreply.github.com>
2023-12-13 10:48:07 +0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,12 +8,14 @@ const app = express();
 const port = process.env.PORT || 5000;
 require("dotenv").config();
 
-app.use(
-  cors({
-    origin: ["https://localhost:5173/","https://excellence-six.vercel.app/"],
-    credentials: true,
-  })
-);
+ app.use(function (req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, 
+    Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+      next();
+    });
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
